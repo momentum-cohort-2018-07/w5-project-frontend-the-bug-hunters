3759d85d8120eed67f6b7f9d688d7e28a855b4fd
Clinton Dreisbach <clinton@dreisbach.us>
2018-09-04 14:29:13 -0400

    Moved repeated logic into setupQuestion

--- script.js
+++ script.js
@@ -1,122 +1,36 @@
 let runningScore = 0
 
-document.getElementById('question1').addEventListener('submit', function (e) {
-  e.preventDefault()
-  e.target.classList.add('hidden')
-  let myAnswer = document.querySelector('input[name="selection-set1"]:checked').dataset.score
-  console.log(myAnswer)
+function setupQuestion (currentQuestionId, inputName, nextQuestionId) {
+  document.getElementById(currentQuestionId).addEventListener('submit', function (e) {
+    e.preventDefault()
+    e.target.classList.add('hidden')
+    let myAnswer = document.querySelector('input[name="' + inputName + '"]:checked').dataset.score
+    console.log(myAnswer)
 
-  runningScore += parseInt(myAnswer, 10)
+    runningScore += parseInt(myAnswer, 10)
 
-  console.log(runningScore, typeof runningScore)
+    console.log(runningScore, typeof runningScore)
 
-  document.getElementById('question2').classList.remove('hidden')
-})
+    if (nextQuestionId) {
+      document.getElementById(nextQuestionId).classList.remove('hidden')
+    } else {
+      document.getElementById('results').innerText = ending(runningScore)
+      document.getElementById('results-pic').innerHTML = endingPhoto(runningScore)
+      document.getElementById('answers-page').classList.remove('hidden')
+    }
+  })
+}
 
-document.getElementById('question2').addEventListener('submit', function (e) {
-  e.preventDefault()
-  e.target.classList.add('hidden')
-  let myAnswer = document.querySelector('input[name="selection-set2"]:checked').dataset.score
-  console.log(myAnswer)
-
-  runningScore += parseInt(myAnswer, 10)
-
-  console.log(runningScore, typeof runningScore)
-  document.getElementById('question3').classList.remove('hidden')
-})
-
-document.getElementById('question3').addEventListener('submit', function (e) {
-  e.preventDefault()
-  e.target.classList.add('hidden')
-  let myAnswer = document.querySelector('input[name="selection-set3"]:checked').dataset.score
-  console.log(myAnswer)
-
-  runningScore += parseInt(myAnswer, 10)
-  console.log(runningScore, typeof runningScore)
-  document.getElementById('question4').classList.remove('hidden')
-})
-
-document.getElementById('question4').addEventListener('submit', function (e) {
-  e.preventDefault()
-  e.target.classList.add('hidden')
-  let myAnswer = document.querySelector('input[name="selection-set4"]:checked').dataset.score
-  console.log(myAnswer)
-
-  runningScore += parseInt(myAnswer, 10)
-  console.log(runningScore, typeof runningScore)
-  document.getElementById('question5').classList.remove('hidden')
-})
-
-document.getElementById('question5').addEventListener('submit', function (e) {
-  e.preventDefault()
-  e.target.classList.add('hidden')
-  let myAnswer = document.querySelector('input[name="selection-set5"]:checked').dataset.score
-  console.log(myAnswer)
-
-  runningScore += parseInt(myAnswer, 10)
-  console.log(runningScore, typeof runningScore)
-  document.getElementById('question6').classList.remove('hidden')
-})
-
-document.getElementById('question6').addEventListener('submit', function (e) {
-  e.preventDefault()
-  e.target.classList.add('hidden')
-  let myAnswer = document.querySelector('input[name="selection-set6"]:checked').dataset.score
-  console.log(myAnswer)
-
-  runningScore += parseInt(myAnswer, 10)
-  console.log(runningScore, typeof runningScore)
-  document.getElementById('question7').classList.remove('hidden')
-})
-
-document.getElementById('question7').addEventListener('submit', function (e) {
-  e.preventDefault()
-  e.target.classList.add('hidden')
-  let myAnswer = document.querySelector('input[name="selection-set7"]:checked').dataset.score
-  console.log(myAnswer)
-  runningScore += parseInt(myAnswer, 10)
-  console.log(runningScore, typeof runningScore)
-  document.getElementById('question8').classList.remove('hidden')
-})
-
-document.getElementById('question8').addEventListener('submit', function (e) {
-  e.preventDefault()
-  e.target.classList.add('hidden')
-  let myAnswer = document.querySelector('input[name="selection-set8"]:checked').dataset.score
-  console.log(myAnswer)
-
-  runningScore += parseInt(myAnswer, 10)
-  console.log(runningScore, typeof runningScore)
-
-  document.getElementById('question9').classList.remove('hidden')
-})
-
-document.getElementById('question9').addEventListener('submit', function (e) {
-  e.preventDefault()
-  e.target.classList.add('hidden')
-  let myAnswer = document.querySelector('input[name="selection-set9"]:checked').dataset.score
-  console.log(myAnswer)
-
-  runningScore += parseInt(myAnswer, 10)
-  console.log(runningScore, typeof runningScore)
-
-  document.getElementById('question10').classList.remove('hidden')
-})
-
-document.getElementById('question10').addEventListener('submit', function (e) {
-  e.preventDefault()
-  e.target.classList.add('hidden')
-  let myAnswer = document.querySelector('input[name="selection-set10"]:checked').dataset.score
-  console.log(myAnswer)
-
-  runningScore += parseInt(myAnswer, 10)
-  console.log(runningScore, typeof runningScore)
-
-  document.getElementById('results').innerText = ending(runningScore)
-  document.getElementById('results-pic').innerHTML = endingPhoto(runningScore)
-
-  document.getElementById('answers-page').classList.remove('hidden')
-})
+setupQuestion('question1', 'selection-set1', 'question2')
+setupQuestion('question2', 'selection-set2', 'question3')
+setupQuestion('question3', 'selection-set3', 'question4')
+setupQuestion('question4', 'selection-set4', 'question5')
+setupQuestion('question5', 'selection-set5', 'question6')
+setupQuestion('question6', 'selection-set6', 'question7')
+setupQuestion('question7', 'selection-set7', 'question8')
+setupQuestion('question8', 'selection-set8', 'question9')
+setupQuestion('question9', 'selection-set9', 'question10')
+setupQuestion('question10', 'selection-set10')
 
 document.getElementById('start-over').addEventListener('click', function () {
   window.location.reload()
